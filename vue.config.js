4b73ed3cfce707afae8c13d242a847933851d9ee
freuwoert <33777960+freuwoert@users.noreply.github.com>
2023-08-31 13:30:24 +0200

    ~ package ready: 1.0.0-alpha.0

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: '/',
+    css: {
+        extract: false,
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                vue: path.resolve('./node_modules/vue')
+            },
+        },
+    },
 })
